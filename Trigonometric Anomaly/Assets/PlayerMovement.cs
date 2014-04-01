2f9d8624c1193268f939f8777fbfa1b51dce82f0
shuynh <simon.huynh.cem@gmail.com>
2014-04-01 17:23:27 -0400

    PlayerMovement and ThirdPersonCamera

---
+++ Trigonometric Anomaly/Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 0.5f;
+	public bool hMouseMovement = false;
+	public bool vMouseMovement = false;
+	public float horizontalSpeed = 4.0f;
+	public float verticalSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Rotation transformation
+		float hAngle = 0;
+		float vAngle = 0;
+
+		if (hMouseMovement) 
+		{
+			hAngle = horizontalSpeed * Input.GetAxis ("Mouse X");
+		}
+		if (vMouseMovement) 
+		{
+			vAngle = verticalSpeed * Input.GetAxis ("Mouse Y");
+		}
+		transform.Rotate (vAngle, hAngle, 0);
+
+		//Translation transformation
+		if (Input.GetKey (KeyCode.W))
+		{
+			this.transform.Translate(new Vector3(0f, 0f, speed));
+		}
+		if (Input.GetKey (KeyCode.A))
+		{
+			this.transform.Translate(new Vector3(-speed, 0f, 0f));
+		}
+		if (Input.GetKey (KeyCode.S))
+		{
+			this.transform.Translate(new Vector3(0f, 0f, -speed));
+		}
+		if (Input.GetKey (KeyCode.D))
+		{
+			this.transform.Translate(new Vector3(speed, 0f, 0f));	
+		}
+	}
+}
---
+++ Trigonometric Anomaly/Assets/ThirdPersonCamera.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	public enum enumCameraType 
+	{
+		OrbitingCamera,
+		FollowBehindCamera,
+		PanningCamera
+	}
+
+	public enumCameraType cameraType = enumCameraType.OrbitingCamera;
+
+	public GameObject targetObject;
+	private Transform target;
+
+	public float distance_behind_target = 6.0f;
+	public float distance_above_target = 2.0f;
+	public float distanceForOrbitCamera = 7.0f;
+    public float translationRate = 100.0f;
+	public bool hPanning = false;
+	public bool vPanning = false;
+	public float horizontalSpeed = 4.0f;
+	public float verticalSpeed = 4.0f;
+	public bool lockLookAtCharacter = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		target = targetObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void LateUpdate()
+	{
+		switch (cameraType) 
+		{
+			case enumCameraType.OrbitingCamera:
+				OrbitingUpdate();
+				break;
+			case enumCameraType.FollowBehindCamera:
+				FollowBehindUpdate();
+				break;
+			case enumCameraType.PanningCamera:
+				PanningUpdate();
+				break;
+			default:
+				break;
+		}
+
+		if(target != null && lockLookAtCharacter)
+        	this.transform.LookAt(target.position);
+	}
+
+	private void PanningUpdate()
+	{
+		//Rotation transformation
+		float hAngle = 0;
+		float vAngle = 0;
+		
+		if (hPanning) 
+		{
+			hAngle = horizontalSpeed * Input.GetAxis ("Mouse X");
+		}
+		if (vPanning) 
+		{
+			vAngle = verticalSpeed * Input.GetAxis ("Mouse Y");
+		}
+
+		transform.Rotate (vAngle, hAngle, 0);
+	}
+
+	private void FollowBehindUpdate()
+	{
+		float delta_time = Time.deltaTime;
+		
+		//Translation transformation
+		Vector3 cameraVectZ = (-target.forward * distance_behind_target);
+		Vector3 cameraVectY = target.up * distance_above_target;
+		
+		//Debugging sequence
+		Debug.DrawLine(target.position + cameraVectZ, target.position, Color.blue);
+		Debug.DrawLine(target.position + cameraVectY, target.position, Color.green);
+		Debug.DrawLine(target.position + cameraVectY + cameraVectZ, target.position, Color.cyan);
+		
+		Vector3 newPosition = Vector3.Lerp(this.transform.position, target.position + cameraVectY + cameraVectZ, delta_time * translationRate);
+		
+		this.transform.position = newPosition;
+	}
+
+	private void OrbitingUpdate()
+	{
+		//Rotation transformation
+		float hAngle = 0;
+		float vAngle = 0;
+		
+		if (hPanning) 
+		{
+			hAngle = horizontalSpeed * Input.GetAxis ("Mouse X");
+		}
+		if (vPanning) 
+		{
+			vAngle = verticalSpeed * Input.GetAxis ("Mouse Y");
+		}
+		transform.RotateAround (target.position, this.transform.right, vAngle);
+		transform.RotateAround (target.position, this.transform.up, hAngle);
+
+		//Zooming in/out
+		//Vector3.MoveTowards
+
+	}
+}
