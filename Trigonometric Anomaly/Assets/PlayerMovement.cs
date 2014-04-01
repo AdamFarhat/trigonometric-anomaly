af0f441c185e38a19a1d3701c4d323afaed1e7eb
shuynh <simon.huynh.cem@gmail.com>
2014-04-01 18:52:49 -0400

    Fluid movement/steering, adjusted camera smoothing, adjusted player speed and mass.

--- Trigonometric Anomaly/Assets/PlayerMovement.cs
+++ Trigonometric Anomaly/Assets/PlayerMovement.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	public float speed = 0.5f;
-	public bool hMouseMovement = false;
-	public bool vMouseMovement = false;
-	public float horizontalSpeed = 4.0f;
-	public float verticalSpeed = 4.0f;
+	[SerializeField] private float maxSpeed = 0.5f;
+	[SerializeField] private float maxAngularVelocity = 10f;
+	[SerializeField] public Vector3 currentVelocity = Vector3.zero;
+	[SerializeField] private Vector3 currentDirection = Vector3.zero;
+	[SerializeField] private float mass = 10.0f;
+	[SerializeField] Vector3 steering = Vector3.zero;
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,36 +18,48 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		//Rotation transformation
-		float hAngle = 0;
-		float vAngle = 0;
-
-		if (hMouseMovement) 
-		{
-			hAngle = horizontalSpeed * Input.GetAxis ("Mouse X");
-		}
-		if (vMouseMovement) 
-		{
-			vAngle = verticalSpeed * Input.GetAxis ("Mouse Y");
-		}
-		transform.Rotate (vAngle, hAngle, 0);
+		float delta_time = Time.deltaTime;
+		Vector3 myPosition = this.transform.position;
+		Vector3 targetPosition = this.transform.position;
 
 		//Translation transformation
 		if (Input.GetKey (KeyCode.W))
 		{
-			this.transform.Translate(new Vector3(0f, 0f, speed));
+			targetPosition += new Vector3(0f, 0f, 1f);
 		}
 		if (Input.GetKey (KeyCode.A))
 		{
-			this.transform.Translate(new Vector3(-speed, 0f, 0f));
+			targetPosition += new Vector3(-1f, 0f, 0f);
 		}
 		if (Input.GetKey (KeyCode.S))
 		{
-			this.transform.Translate(new Vector3(0f, 0f, -speed));
+			targetPosition += new Vector3(0f, 0f, -1f);
 		}
 		if (Input.GetKey (KeyCode.D))
 		{
-			this.transform.Translate(new Vector3(speed, 0f, 0f));	
+			targetPosition += new Vector3(1f, 0f, 0f);
 		}
+
+		Vector3 direction = targetPosition - myPosition;
+
+		direction.Normalize();
+
+		if(targetPosition != myPosition)
+		{
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), delta_time * maxAngularVelocity);
+			currentDirection = direction;
+		}
+
+		direction *= maxSpeed;
+		steering = direction - currentVelocity;
+
+		Debug.DrawLine(myPosition, myPosition + this.transform.forward * 5f, Color.black);
+		Debug.DrawLine(myPosition, direction, Color.red);
+		Debug.DrawLine(myPosition, steering, Color.green);
+
+		currentVelocity += steering/mass;
+
+		Vector3 newPosition = myPosition + currentVelocity * delta_time;
+		this.transform.position = newPosition;
 	}
 }
