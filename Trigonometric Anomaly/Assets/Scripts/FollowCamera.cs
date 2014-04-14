de0946bfb877d1cad903d0a876fe7544d8f39187
MysteryUser <madad58@gmail.com>
2014-04-14 15:57:52 -0400

    Added explosions and rearrenged assets folder

---
+++ Trigonometric Anomaly/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour 
+{
+	GameObject camera;
+	
+	void Start () 
+	{
+		camera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		transform.position = camera.transform.position + new Vector3(0,-50,0);
+	}
+}
